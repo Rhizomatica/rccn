deba71f9141bbd94355b839b9b7660ebd4863671
tele <tele0x@gmail.com>
2014-02-01 02:20:54 -0600

    - created Subscription module - created RSC Subscription checker job - created database migration to add handling of subscription, created trigger to update subscription_date on subscription status change - added subscription_status to add subscriber - added change of subscription status to edit subscriber API

---
+++ var/rhizomatica/rccn/modules/subscription.py
@@ -0,0 +1,119 @@
+############################################################################
+# 
+# Copyright (C) 2014 tele <tele@rhizomatica.org>
+#
+# Subscription module
+# This file is part of RCCN
+#
+# RCCN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RCCN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################
+
+import sys
+sys.path.append("..")
+from config import *
+
+from subscriber import Subscriber, SubscriberException 
+from sms import SMS, SMSException
+
+class SubscriptionException(Exception):
+        pass
+
+class Subscription:
+
+	def __init__(self, logger):
+		self.logger = logger
+
+	def get_unpaid_subscriptions(self):
+		# get all subscribers that haven't paid yet
+		try:
+                        cur = db_conn.cursor()
+                        cur.execute('SELECT msisdn FROM subscribers WHERE subscription_status = 0')
+			count = cur.rowcount
+                        if count > 0:
+                                subscribers_list = cur.fetchall()
+				self.logger.info('Found %s subscribers with unpaid subscription to the service' % count)
+				return subscribers_list
+                        else:
+				self.logger.info('PG_HLR Everyone paid, we are good to go')
+                except psycopg2.DatabaseError, e:
+                        raise SubscriptionException('PG_HLR error getting subscribers subscription_status: %s' % e)
+
+
+        def update_subscriptions(self, status):
+                try:
+                        cur = db_conn.cursor()
+                        cur.execute('UPDATE subscribers SET subscription_status=%(status)d' % {'status': status})
+			count = cur.rowcount
+                        if count > 0:
+                                db_conn.commit()
+				return count
+                        else:
+				self.logger.info('PG_HLR No subscribers to update status found')
+                except psycopg2.DatabaseError as e:
+			raise SubscriptionException('PG_HLR error in updating subscriptions status: %s' % e)
+
+	def deactivate_subscriptions(self,msg):
+		try:
+			cur = db_conn.cursor()
+                        cur.execute('SELECT msisdn FROM subscribers WHERE subscription_status = 0')
+                        count = cur.rowcount
+                        if count > 0:
+				self.logger.info('Found %d subscribers to be deactivated' % count)
+                                subscribers_list = cur.fetchall()
+				for mysub in subscribers_list:
+					self.logger.debug('Send SMS that account is deactivated to %s' % mysub[0])
+					#sms.send_immediate(mysub[0],msg)
+				
+				try:
+					cur = db_conn.cursor()
+					cur.execute('UPDATE subscribers SET authorized=0 WHERE subscription_status=0')
+					count = cur.rowcount
+					if count > 0:
+						db_conn.commit()
+						self.logger.info('Subscription deactivated for %d subscribers' % count)
+				except psycopg2.DatabaseError as e:
+					raise SubscriptionException('PG_HLR error in deactivating subscriptions: %s' % e)
+			else:
+				self.logger.info('No subscribers need to be deactivate')
+		except psycopg2.DatabaseError as e:
+			raise SubscriptionException('PG_HLR error in checking subscriptions to deactivate: %s' % e)
+
+
+	def send_subscription_fee_notice(self, msg):
+		# get all subscribers
+		try:
+			sub = Subscriber()
+			subscribers_list = sub.get_all()
+		except SubscriberException as e:
+			raise SubscriptionException('%s' % e)
+
+		sms = SMS()
+
+		for mysub in subscribers_list:
+			self.logger.debug("Send sms to %s %s" % (mysub[1],msg))
+			sms.send_immediate(mysub[1],msg)
+
+	def send_subscription_fee_reminder(self, msg):
+		try:
+			subscribers_list = self.get_unpaid_subscriptions()
+		except SubscriptionException as e:
+			raise SubscribtionException('ERROR in getting unpaid subscriptions')
+
+		sms = SMS()
+		
+		for mysub in subscribers_list:
+			self.logger.debug("Send sms to %s %s" % (mysub[0],msg))
+			sms.send_immeditate(mysub[1],msg)
+	
---
+++ var/rhizomatica/rccn/rsc.py
@@ -0,0 +1,65 @@
+############################################################################
+# 
+# Copyright (C) 2014 tele <tele@rhizomatica.org>
+#
+# Subscription Checker
+# This file is part of RCCN
+#
+# RCCN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# RCCN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################
+
+import sys
+from config import *
+
+notice_msg = 'Notice to pay the subscription fee of 30 pesos'
+reminder_msg = 'Your service will be deactivated if you don\'t pay the subscription'
+deactivate_msg = 'Your service have been deactivated until the subscription fee is paid'
+
+if len(sys.argv) > 2:
+	print 'RCCN Subscription Checker\n'
+	print 'Usage: %s [notice|reminder|deactivate]' % sys.argv[0]
+        sys.exit(1)
+
+
+action = sys.argv[1]
+
+rsc = Subscription(subscription_log)
+
+if action == 'notice':
+	subscription_log.info('Send notification to all subscribers to pay the subscription fee')
+	try:
+		totalsub = rsc.update_subscriptions(0)
+		subscription_log.info('Updated subscription status to unpaid for %d subscribers' % totalsub)
+		subscription_log.info('Send SMS of paying the subscription fee to all subscribers')
+		rsc.send_subscription_fee_notice(notice_msg)
+	except SubscriptionException as e:
+		subscription_log.error('%s' % e)
+elif action == 'reminder':
+	subscription_log.info('Send reminder to all subscribers that haven\'t yet paid the subscription')
+	try:
+		rsc.send_subscription_fee_reminder(reminder_msg)
+	except SubscriptionException as e:
+		subscription_log.error('%s' % e)
+elif action == 'deactivate':
+	subscription_log.info('Deactivate all subscribers that haven\'t paid their subscription fee')
+	try:
+		rsc.deactivate_subscriptions(deactivate_msg)
+	except SubscriptionException as e:
+		subscription_log.error('%s' % e)
+else:
+	subscription_log.info('Invalid option')
+	sys.exit(1)	
+
+
