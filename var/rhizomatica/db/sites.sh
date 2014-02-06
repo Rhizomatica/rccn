46a8cd83cc7044be84d94458c18ea1511395812c
tele <tele@rhizomatica.org>
2014-02-05 19:33:37 -0600

    Added sites bucket initialization for Riak

---
+++ var/rhizomatica/db/sites.sh
@@ -0,0 +1,9 @@
+#"!/bin/bash
+# Populate sites in riak
+
+# Talea
+curl -v -X PUT http://localhost:8098/buckets/sites/keys/688201 -H "Content-Type: application/json" -d '{"site_name": "Talea", "postcode": "68820", "pbxcode": "1", "network_name": "TaleaGSM", "ip_address":"10.66.0.10"}'
+# Yaviche
+curl -v -X PUT http://localhost:8098/buckets/sites/keys/688261 -H "Content-Type: application/json" -d '{"site_name": "Yaviche", "postcode": "68826", "pbxcode": "1", "network_name": "BueXhidza", "ip_address":"10.66.0.34"}'
+
+
