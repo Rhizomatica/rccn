5ce99025a676a0b312cc9e13ebfcab69e5e1e5ee
tele <tele@rhizomatica.org>
2014-02-06 00:52:59 -0600

    Added site-to-site SMS support

--- var/rhizomatica/rccn/modules/sms.py
+++ var/rhizomatica/rccn/modules/sms.py
@@ -48,9 +48,7 @@ class SMS:
 		# check if destination number is local
 		try:
 			numbering = Numbering()
-                        num_res = numbering.is_number_local(destination)
-
-			if num_res == 0:
+			if numbering.is_number_local(destination) == True:
 				log.info('SMS_LOCAL check if subscriber is authorized')
 				# check if subscriber is authorized
 				try:
@@ -64,36 +62,53 @@ class SMS:
 						return
 				except SubscriberException as e:
 					log.error(e)
-			elif num_res == 1:
-				# number is internal send SMS to correct site
-				return
 			else:
 		
 				# dest number is not local, check if dest number is a shortcode
 				if destination in extensions_list:
 					log.info('Destination number is a shortcode, execute shortcode handler')
-					shortcode = importlib.import_module('extensions.ext_'+self.destination, 'extensions')
+					shortcode = importlib.import_module('extensions.ext_'+destination, 'extensions')
 					try:
 						log.debug('Exec shortcode handler')
 						extension.handler('',source, destination, text)
 					except ExtensionException as e:
 						log.error(e)
 				else:
-				# dest number is for an external number send sms to sms provider
-					return
+					# check if sms is for another location
+					if (self.numbering.is_number_internal(destination) == True and len(destination) == 11:
+						log.info('SMS is for another site')
+						try:
+							site_ip = self.numbering.get_site_ip(destination)
+							log.info('Send SMS to site IP: %s' % site_ip)
+							self.send(source,destination,text,site_ip)
+						except NumberingException as e:
+							log.error(e)
+					else:
+						# dest number is for an external number send sms to sms provider
+						return
 		except NumberingException as e:
 			log.error(e)
 	
 	def send(self, source, destination, text, server='localhost'):
-		log.info('Send SMS: %s %s %s' % (source, destination, text))
 		enc_text = urllib.quote(text)
-		try:
-                        res = urllib.urlopen(
-                                "http://%s:%d/cgi-bin/sendsms?username=%s&password=%s&charset=%s&coding=%d&to=%s&from=%s&text=%s"\
-                                % (server, self.port, self.username, self.password, self.charset, self.coding, destination, source, enc_text)
-                        ).read()
-                except IOError:
-                        raise SMSException('Error connecting to Kannel to send SMS: %s' % e)
+		if server != 'localhost':
+			try:
+				log.info('Send SMS: %s %s %s' % (source, destination, text))
+        	                res = urllib.urlopen(
+                	                "http://%s:%d/cgi-bin/sendsms?username=%s&password=%s&charset=%s&coding=%d&to=%s&from=%s&text=%s"\
+                        	        % (server, self.port, self.username, self.password, self.charset, self.coding, destination, source, enc_text)
+	                        ).read()
+        	        except IOError:
+                	        raise SMSException('Error connecting to Kannel to send SMS: %s' % e)
+		else:
+			try:
+				log.info('Send SMS to %s: %s %s %s' % (server, source, destination, text))
+        	                res = urllib.urlopen(
+				values = {'source': source, 'destination': destination, 'text': text }
+				res = urllib.urlopen('http://%s:8085/sms' % server,values).read()
+			except IOError:
+				raise SMSException('Error sending SMS to site %s' % server)
+				
 
 	def send_immediate(self,num,text):
 		appstring = "OpenBSC"
@@ -104,7 +119,6 @@ class SMS:
 
 
 	
-
 if __name__ == '__main__':
 	sms = SMS()
 	#sub.set_balance('68820110010',3.86)
