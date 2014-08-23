aae8ccb8d447c825bc2085b2e7a4f9f6c4dc2cc0
Ciaby <ciaby@rhizomatica.org>
2014-08-23 12:45:08 -0500

    Moved/deleted stuff, now it's only the content of /var/rhizomatica

--- rmai/cdr_processing.php
+++ rmai/cdr_processing.php
@@ -1,159 +1,197 @@
 <?php
-	$aColumns = array( 'AcctId', 'calldate', 'clid', 'dcontext', 'channel', 'dstchannel', 'lastapp', 'lastdata', 'duration', 'billsec', 'disposition', 'amaflags', 'accountcode' );
-	
-	/* Indexed column (used for fast and accurate table cardinality) */
-	$sIndexColumn = "AcctId";
-	
-	/* DB table to use */
-	$sTable = "cdr";
-	$database = new PDO('sqlite:db/master.sqlite');
-		
-	/* 
-	 * Paging
-	 */
-	$sLimit = "";
-	if ( isset( $_GET['iDisplayStart'] ) && $_GET['iDisplayLength'] != '-1' )
-	{
-		$sLimit = "LIMIT ".intval( $_GET['iDisplayStart'] ).", ".
-			intval( $_GET['iDisplayLength'] );
-	}
-	
-	
-	/*
-	 * Ordering
-	 */
-	$sOrder = "";
-	if ( isset( $_GET['iSortCol_0'] ) )
-	{
-		$sOrder = "ORDER BY  ";
-		for ( $i=0 ; $i<intval( $_GET['iSortingCols'] ) ; $i++ )
-		{
-			if ( $_GET[ 'bSortable_'.intval($_GET['iSortCol_'.$i]) ] == "true" )
-			{
-				$sOrder .= "`".$aColumns[ intval( $_GET['iSortCol_'.$i] ) ]."` ".
-					($_GET['sSortDir_'.$i]==='asc' ? 'asc' : 'desc') .", ";
-			}
-		}
-		
-		$sOrder = substr_replace( $sOrder, "", -2 );
-		if ( $sOrder == "ORDER BY" )
-		{
-			$sOrder = "";
-		}
-	}
-	
-	
-	/* 
-	 * Filtering
-	 * NOTE this does not match the built-in DataTables filtering which does it
-	 * word by word on any field. It's possible to do here, but concerned about efficiency
-	 * on very large tables, and MySQL's regex functionality is very limited
-	 */
-	$sWhere = "";
-	if ( isset($_GET['sSearch']) && $_GET['sSearch'] != "" )
-	{
-		$sWhere = "WHERE (";
-		for ( $i=0 ; $i<count($aColumns) ; $i++ )
-		{
-			//if ( isset($_GET['bSearchable_'.$i]) && $_GET['bSearchable_'.$i] == "true" )
-			//{
-				$sWhere .= "`".$aColumns[$i]."` LIKE '%".$_GET['sSearch']."%' OR ";
-			//}
-		}
-		$sWhere = substr_replace( $sWhere, "", -3 );
-		$sWhere .= ')';
-	}
-	
-	/* Individual column filtering */
-	for ( $i=0 ; $i<count($aColumns) ; $i++ )
-	{
-		if ( isset($_GET['bSearchable_'.$i]) && $_GET['bSearchable_'.$i] == "true" && $_GET['sSearch_'.$i] != '' )
-		{
-			if ( $sWhere == "" )
-			{
-				$sWhere = "WHERE ";
-			}
-			else
-			{
-				$sWhere .= " AND ";
-			}
-			$sWhere .= "`".$aColumns[$i]."` LIKE '%".$_GET['sSearch_'.$i]."%' ";
-		}
-	}
-	
-	
-	/*
-	 * SQL queries
-	 * Get data to display
-	 */
-	//$sQuery = "
-	//	SELECT SQL_CALC_FOUND_ROWS `".str_replace(" , ", " ", implode("`, `", $aColumns))."`
-	$sQuery = "SELECT ".implode(",", $aColumns)."
-		FROM   $sTable
-		$sWhere
-		$sOrder
-		$sLimit
-		";
-	//echo "QUERY: $sQuery";
-	$rResult = $database->query($sQuery);
 
-	$sQuery = "SELECT count(*) FROM   $sTable
-		$sWhere
-		$sOrder
-		$sLimit
-		";
-	$result = $database->query($sQuery);
-	$row = $result->fetch(PDO::FETCH_NUM);
-	$iFilteredTotal = $row[0];//$rResult->rowCount();
-	//print_r($iFilteredTotal);
-	/* Data set length after filtering */
-	/*$sQuery = "
-		SELECT FOUND_ROWS()
-	";
-	$rResultFilterTotal = mysql_query( $sQuery, $gaSql['link'] ) or fatal_error( 'MySQL Error: ' . mysql_errno() );
-	$aResultFilterTotal = mysql_fetch_array($rResultFilterTotal);
-	$iFilteredTotal = $aResultFilterTotal[0];*/
-	
-	/* Total data set length */
-	$sQuery = "
-		SELECT COUNT(`".$sIndexColumn."`)
-		FROM   $sTable
-	";
-	$rResultTotal = $database->query($sQuery);
-	$aResultTotal = $rResultTotal->fetch();
-	$iTotal = $aResultTotal[0];
-	
-	
-	/*
-	 * Output
-	 */
-	
-	$sEcho = isset($_GET['sEcho']) ? $_GET['sEcho'] : 1;
-	$output = array(
-		"sEcho" => intval($sEcho),
-		"iTotalRecords" => $iTotal,
-		"iTotalDisplayRecords" => $iFilteredTotal,
-		"aaData" => array()
-	);
-	
-	while ( $aRow = $rResult->fetch(SQLITE_ASSOC) )
-	{
-		$row = array();
-		for ( $i=0 ; $i<count($aColumns) ; $i++ )
-		{
-			if ( $aColumns[$i] == "version" )
-			{
-				/* Special output formatting for 'version' column */
-				$row[] = ($aRow[ $aColumns[$i] ]=="0") ? '-' : $aRow[ $aColumns[$i] ];
-			}
-			else if ( $aColumns[$i] != ' ' )
-			{
-				/* General output */
-				$row[] = $aRow[ $aColumns[$i] ];
-			}
-		}
-		$output['aaData'][] = $row;
-	}
-	
-	echo json_encode( $output );
+    require_once('include/database.php');
+
+    /*
+     * Script:    DataTables server-side script for PHP and PostgreSQL
+     * Copyright: 2010 - Allan Jardine
+     * License:   GPL v2 or BSD (3-point)
+     */
+     
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+     * Easy set variables
+     */
+     
+    /* Array of database columns which should be read and sent back to DataTables. Use a space where
+     * you want to insert a non-database field (for example a counter or static image)
+     */
+    $aColumns = array( 'id', 'start_stamp', 'caller_id_number', 'destination_number', 'context', 'billsec', 'hangup_cause', 'destination_name', 'cost' );
+     
+    /* Indexed column (used for fast and accurate table cardinality) */
+    $sIndexColumn = "id";
+     
+    /* DB table to use */
+    $sTable = "cdr";
+     
+     
+     
+    $gaSql['user']       = $DB_USER;
+    $gaSql['password']   = $DB_PASSWORD;
+    $gaSql['db']         = $DB_DATABASE;
+    $gaSql['server']     = $DB_HOST;
+
+    /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+     * If you just want to use the basic configuration for DataTables with PHP server-side, there is
+     * no need to edit below this line
+     */
+     
+    /*
+     * DB connection
+     */
+    $gaSql['link'] = pg_connect(
+        " host=".$gaSql['server'].
+        " dbname=".$gaSql['db'].
+        " user=".$gaSql['user'].
+        " password=".$gaSql['password']
+    ) or die('Could not connect: ' . pg_last_error());
+     
+     
+    /*
+     * Paging
+     */
+    $sLimit = "";
+    if ( isset( $_GET['iDisplayStart'] ) && $_GET['iDisplayLength'] != '-1' )
+    {
+        $sLimit = "LIMIT ".intval( $_GET['iDisplayLength'] )." OFFSET ".
+            intval( $_GET['iDisplayStart'] );
+    }
+     
+     
+    /*
+     * Ordering
+     */
+    if ( isset( $_GET['iSortCol_0'] ) )
+    {
+        $sOrder = "ORDER BY  ";
+        for ( $i=0 ; $i<intval( $_GET['iSortingCols'] ) ; $i++ )
+        {
+            if ( $_GET[ 'bSortable_'.intval($_GET['iSortCol_'.$i]) ] == "true" )
+            {
+                $sOrder .= $aColumns[ intval( $_GET['iSortCol_'.$i] ) ]."
+                    ".($_GET['sSortDir_'.$i]==='asc' ? 'asc' : 'desc').", ";
+            }
+        }
+         
+        $sOrder = substr_replace( $sOrder, "", -2 );
+        if ( $sOrder == "ORDER BY" )
+        {
+            $sOrder = "";
+        }
+    }
+     
+     
+    /*
+     * Filtering
+     * NOTE This assumes that the field that is being searched on is a string typed field (ie. one
+     * on which ILIKE can be used). Boolean fields etc will need a modification here.
+     */
+    $sWhere = "";
+    if ( $_GET['sSearch'] != "" )
+    {
+        $sWhere = "WHERE (";
+        for ( $i=0 ; $i<count($aColumns) ; $i++ )
+        {
+            if ( $_GET['bSearchable_'.$i] == "true" )
+            {
+                $sWhere .= "CAST(".$aColumns[$i]." AS TEXT) ILIKE '%".pg_escape_string( $_GET['sSearch'] )."%' OR ";
+            }
+        }
+        $sWhere = substr_replace( $sWhere, "", -3 );
+        $sWhere .= ")";
+    }
+     
+    /* Individual column filtering */
+    for ( $i=0 ; $i<count($aColumns) ; $i++ )
+    {
+        if ( $_GET['bSearchable_'.$i] == "true" && $_GET['sSearch_'.$i] != '' )
+        {
+            if ( $sWhere == "" )
+            {
+                $sWhere = "WHERE ";
+            }
+            else
+            {
+                $sWhere .= " AND ";
+            }
+            $sWhere .= "CAST(".$aColumns[$i]." AS TEXT) ILIKE '%".pg_escape_string($_GET['sSearch_'.$i])."%' ";
+        }
+    }
+     
+     
+    $sQuery = "
+        SELECT ".str_replace(" , ", " ", implode(", ", $aColumns))."
+        FROM   $sTable
+        $sWhere
+        $sOrder
+        $sLimit
+    ";
+    $rResult = pg_query( $gaSql['link'], $sQuery ) or die(pg_last_error());
+     
+    $sQuery = "
+        SELECT $sIndexColumn
+        FROM   $sTable
+    ";
+    $rResultTotal = pg_query( $gaSql['link'], $sQuery ) or die(pg_last_error());
+    $iTotal = pg_num_rows($rResultTotal);
+    pg_free_result( $rResultTotal );
+     
+    if ( $sWhere != "" )
+    {
+        $sQuery = "
+            SELECT $sIndexColumn
+            FROM   $sTable
+            $sWhere
+        ";
+        $rResultFilterTotal = pg_query( $gaSql['link'], $sQuery ) or die(pg_last_error());
+        $iFilteredTotal = pg_num_rows($rResultFilterTotal);
+        pg_free_result( $rResultFilterTotal );
+    }
+    else
+    {
+        $iFilteredTotal = $iTotal;
+    }
+     
+     
+     
+    /*
+     * Output
+     */
+    $output = array(
+        "sEcho" => intval($_GET['sEcho']),
+        "iTotalRecords" => $iTotal,
+        "iTotalDisplayRecords" => $iFilteredTotal,
+        "aaData" => array()
+    );
+     
+    while ( $aRow = pg_fetch_array($rResult, null, PGSQL_ASSOC) )
+    {
+        $row = array();
+        for ( $i=0 ; $i<count($aColumns) ; $i++ )
+        {
+            if ( $aColumns[$i] == "version" )
+            {
+		$row[] =  date('d-m-Y H:i:s', strtotime($aRow[$aColumns[$i]]));
+            }
+	    if ( $aColumns[$i] == "billsec") {
+		$row[] = gmdate("H:i:s", $aRow[ $aColumns[$i] ]);
+	    }
+	    else if ( $aColumns[$i] == "start_stamp" ) {
+		$row[] =  date('d-m-Y H:i:s', strtotime($aRow[$aColumns[$i]]));
+	    }
+            else if ( $aColumns[$i] != ' ' )
+            {
+                /* General output */
+                $row[] = $aRow[ $aColumns[$i] ];
+            }
+        }
+        $output['aaData'][] = $row;
+    }
+     
+    echo json_encode( $output );
+     
+    // Free resultset
+    pg_free_result( $rResult );
+     
+    // Closing connection
+    pg_close( $gaSql['link'] );
 ?>
