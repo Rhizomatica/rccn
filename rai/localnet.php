e2cb6e10e72de1589d6eba41fb3f2126e8c380a9
dave onion <multi.lectical@gmail.com>
2015-08-26 17:12:30 -0500

    localnet.php

---
+++ rai/localnet.php
@@ -0,0 +1,18 @@
+<?php 
+require_once('include/header.php');
+require_once('include/menu.php'); 
+
+
+// print_menu('platform');
+ ?>
+<link rel="stylesheet" href="css/leaflet.css" />
+<script src="js/leaflet.js"></script>
+
+	<div id="map"></div>
+	<div id="outsideWorld"></div>
+
+</div>
+<script src="js/localnet.js"></script>
+</body>
+
+</html>
