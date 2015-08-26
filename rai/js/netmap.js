3bf1cda2dbe21e25504161857f633dbf38418aa4
dave onion <multi.lectical@gmail.com>
2015-08-26 17:11:53 -0500

    netmap.js

---
+++ rai/js/netmap.js
@@ -0,0 +1,142 @@
+jQuery(document).ready(function($) {
+
+    var status = "maybe",
+        lats = [],
+        longs = [],
+        type, ip, geo, status, deviceIcon, marker, stats, zoom, //keep vars global
+
+        btsIcon = L.icon({
+            iconUrl: 'img/bts.png',
+            iconSize: [45, 45], // size of the icon
+            iconAnchor: [20, -20], // point of the icon which will correspond to marker's location
+            popupAnchor: [-5, 20] // point from which the popup should open relative to the iconAnchor
+        }),
+        bscIcon = L.icon({
+            iconUrl: 'img/bsc.png',
+            iconSize: [45, 45], // size of the icon
+            iconAnchor: [20, -20], // point of the icon which will correspond to marker's location
+            popupAnchor: [30, 5] // point from which the popup should open relative to the iconAnchor
+        }),
+        linkIcon = L.icon({
+            iconUrl: 'img/link.png',
+            iconSize: [45, 45], // size of the icon
+            iconAnchor: [0, 10], // point of the icon which will correspond to marker's location
+            popupAnchor: [5, -5] // point from which the popup should open relative to the iconAnchor
+        }),
+
+        // standard things to check
+        standardChecks = {
+            "Servidor": "10.23.0.2",
+            "VOIP": "169.132.196.11",
+            "Google": "8.8.8.8"
+        };
+
+    for (var key in standardChecks) {
+        ip = standardChecks[key];
+
+        pingIt(ip, key);
+    }
+
+    // set up the initial map
+    var map = L.map('map').setView([ 17.066409, -96.729473], 2);
+
+    // get json data
+    var network = $.getJSON("netmap.json", function(data) {}).fail(function() {
+        console.log("Can't find any devices???");
+    });
+
+    // when done getting json make a map
+    network.done(function(net) {
+
+        // do this: http://stackoverflow.com/questions/17277686/leaflet-js-center-the-map-on-a-group-of-markers
+
+        // get the tiles from mapBox
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 18,
+            id: 'multilectical.n3mbdga4',
+            accessToken: 'pk.eyJ1IjoibXVsdGlsZWN0aWNhbCIsImEiOiJiZmQ3NzViNGI3NTBhNzdiMzk0NGI3ZmUyMTVhZjgzMSJ9.K0fI8HFCQvmHqk0S9RY4Sw'
+        }).addTo(map);
+
+
+        // loop through devices
+        for (var key in net) {
+            if (net.hasOwnProperty(key)) {
+
+                // make vars of all the things
+                type = net[key].type,
+                ip = net[key].ip,
+                geo = net[key].geo;
+
+                // use the opportunity to make arrays of boundaries for later
+                lats.push(net[key].geo[0]);
+                longs.push(net[key].geo[1]);
+
+                // throw the ip to php
+                status = pingStatus(ip, type, geo);
+
+            }
+
+        }
+
+        // resize map
+
+        var zoom = .006,
+            enlarge = largestLat = Math.max.apply(Math, lats) + zoom,
+            smallestLat = Math.min.apply(Math, lats) - zoom,
+            largestLon = Math.max.apply(Math, longs) + zoom,
+            smallestLon = Math.min.apply(Math, longs) - zoom;
+        console.log("largestLat: " + largestLat + " smallestLat: " + smallestLat + " largestLon: " + largestLon + " smallestLon: " + smallestLon);
+
+
+        var southWest = L.latLng(smallestLat, smallestLon),
+            northEast = L.latLng(largestLat, largestLon),
+            bounds = L.latLngBounds(southWest, northEast);
+        map.fitBounds(bounds);
+
+    });
+
+    function pingStatus(ip, type, geo) {
+
+        $.ajax({
+            method: "POST",
+            url: "ping.php",
+            data: {
+                ip
+            }
+        })
+            .done(function(status) {
+
+                //make an iconout of the type var
+                deviceIcon = eval(type.concat("Icon"));
+
+                marker = L.marker(geo, {
+                    icon: deviceIcon
+                }, {
+                    title: ip
+                }).addTo(map).bindPopup(type + " <br>" + ip + "<br> This is " + status);
+
+                $(marker._icon).addClass(status);
+
+                console.log(ip + ": " + status);
+
+            })
+            .fail(function() {
+                console.log("failaed to ajjjaaxx");
+            });
+    }
+
+    function pingIt(ip, key) {
+        $.ajax({
+            method: "POST",
+            url: "ping.php",
+            data: {
+                ip
+            }
+        }).done(function(status) {
+            console.log("key: " + key + " ip: " + ip + " status: " + status);
+            $("#outsideWorld").append('<div class="' + status + '"><p>' + key + '<br/>' + ip + '</p></div>');
+        });
+    }
+
+});
