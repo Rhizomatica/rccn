1321d74b45601702119a1a5f377090e69cc4e511
Keith <keith@rhizomatica.org>
2016-11-10 01:17:54 +0100

    Turns out we in deployment, we don't have a standard for partitioning. This commit stops some annoying error generation, also most of the time, we would be more interested in whatever is mounted on /var

--- bin/platform_update_rrd.sh
+++ bin/platform_update_rrd.sh
@@ -8,7 +8,7 @@ rrdtool update $RHIZO_DIR/loadaverage.rrd N:$loadaverage
 cpustat=`cat /proc/stat|head -1| sed "s/^cpu\ \+\([0-9]*\)\ \([0-9]*\)\ \([0-9]*\)\ \([0-9]*\).*/\1:\2:\3:\4/"`
 rrdtool update $RHIZO_DIR/cpu.rrd N:$cpustat
 
-temperature=`sensors | grep temp1: | head -1 | awk '{print $2}' | sed -e 's/°C//g' -e 's/\+//g'`
+temperature=`sensors 2>/dev/null | grep temp1: | head -1 | awk '{print $2}' | sed -e 's/°C//g' -e 's/\+//g'`
 rrdtool update $RHIZO_DIR/temperature.rrd N:$temperature
 
 C=$(egrep ^Cached /proc/meminfo|awk '{print $2}')
@@ -20,7 +20,7 @@ SF=$(egrep ^SwapFree /proc/meminfo|awk '{print $2}')
 memupdate="${C}:${B}:${F}:${T}:${ST}:${SF}"
 rrdtool update $RHIZO_DIR/memory.rrd N:$memupdate
 
-DF=$(df -m|grep sda1 |awk '{print $2":"$3}')
+DF=$(df -m|grep /var |awk '{print $2":"$3}')
 #DI=$(cat /proc/partitions|grep sda1| awk '{print $5":"$6":"$7":"$8":"$9":"$10":"$11":"$12":"$13":"$14":"$15}'
 rrdtool update $RHIZO_DIR/disk.rrd N:$DF
 
