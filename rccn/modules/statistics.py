a10fd5e34fe9f7600660928f7a35ed9cb7fba2f6
Keith <keith@rhizomatica.org>
2017-02-11 16:42:09 +0100

    Don't count the CDR when getting stats

--- rccn/modules/statistics.py
+++ rccn/modules/statistics.py
@@ -110,6 +110,8 @@ class LiveStatistics:
             raise StatisticException('SQ_HLR error: %s' % e.args[0])
 
     def get_recent_call_count(self,ago):
+        # This is too hard on PS on sites with largc cdr. need to clean up 1st
+        return 0
         try:
             cur = db_conn.cursor()
             cur.execute("SELECT count (id) FROM cdr WHERE end_stamp > current_timestamp - interval %(ago)s", { 'ago': ago } )
