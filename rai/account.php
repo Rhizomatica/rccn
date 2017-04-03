4ae4ad4650309b51d3f356afa757880da6d40a75
Keith <keith@rhizomatica.org>
2017-04-03 16:34:00 +0200

    Fix display of Credit in account.php

--- rai/account.php
+++ rai/account.php
@@ -37,13 +37,13 @@ foreach ($c_alloc as $row) {
 			    },
 		            success: function (html) {
                         rate=<?=$rate?>;
-                        mxn=html*rate
+                        mxn=parseInt(html*rate)
 			            $('#balance').html("<span style='font-size: 20px;'><?=_('VOIP Account Balance:')?> <b>" + html + "</b> USD / <b>" + mxn + "</b> MXN</span>");
-			            total=parseInt(<?=$c_both?>)
+			            total=<?=number_format(($c_both+$c_not_paid_auth),2,'.','')?>;
 			            diff=(mxn-total)
 			            c="green"
 			            if (diff<0) c="red"
-			            $('#diff').html('<?=_('Credit Status')?>: <span style="color:'+c+';font-weight:bold">' + Math.abs(html-total).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') + '</span> MXN');
+			            $('#diff').html('<?=_('Credit Status')?>: <span style="color:'+c+';font-weight:bold">' + Math.abs(diff).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') + '</span> MXN');
 		            }
 			 });
 			
@@ -56,14 +56,14 @@ foreach ($c_alloc as $row) {
 				echo "<img src='img/account.png' width='150' height='150' /><br/><br/>";
 				echo "<div id='balance'></div>";
 			?>
-			<div id='credit' style="font-size: 16px;text-align: left; width: 400px; margin-top: 10px; border-style: inset; border-width: 1px; border-radius: 7px;padding: 11px;"> 
+			<div id='credit' style="font-size: 16px;text-align: left; width: 420px; margin-top: 10px; border-style: inset; border-width: 1px; border-radius: 7px;padding: 11px;"> 
 			<?=_('Credit Allocated')?>:
 			<div>
 				<div><?=_('Authorised')?>: <b><?=number_format($c_both)?></b> MXN</div>
 				<div><?=_('Not Authorised')?>: <b><?=number_format($c_na)?></b> MXN</div>
 				<div><?=_('Not Paid, Authorised')?>: <b><?=number_format($c_not_paid_auth)?></b> MXN</div>
 				<br />
-				<div><?=_('Total Sold to Authorised Users')?>: <b><span id="ctotal"><?=number_format($c_both + $c_not_paid_auth)?></span></b> 
+				<div><?=_('Total Sold to Authorised Users')?>: <b><span id="ctotal"><?=number_format($c_both + $c_not_paid_auth,2)?></span></b> 
 				MXN</div>
 				<br />
 				<div id="diff"></div>
