f8d8c22177d3cdfba3e27a7a40c84521df095e4a
Keith <keith@rhizomatica.org>
2017-07-28 13:00:54 +0200

    sms_cleanup: Leave unsent messages in the Q a few days!

--- bin/sms_cleanup.sh
+++ bin/sms_cleanup.sh
@@ -13,14 +13,14 @@ logc "Run database cleanup. Current DB size: `ls -sh $SMS_DB | awk '{print $1}'`
 logc "Make backup copy of SMS db"
 #cp -f $SMS_DB $SMS_DB_BKP
 
-total_sms=`echo 'select count(*) from SMS;' | sqlite3 $SMS_DB`
-total_sms_delivered=`echo 'select count(*) from SMS where sent is not null;' | sqlite3 $SMS_DB`
-total_sms_old=`echo "select count(*) from SMS where created < datetime('now', '-1 day');" | sqlite3 $SMS_DB`
+total_sms=`echo 'select count(*) from SMS;' | sqlite3 -init <(echo .timeout 1000) $SMS_DB`
+total_sms_delivered=`echo 'select count(*) from SMS where sent is not null;' | sqlite3 -init <(echo .timeout 1000) $SMS_DB`
+total_sms_old=`echo "select count(*) from SMS where created < datetime('now', '-4 day');" | sqlite3 -init <(echo .timeout 1000) $SMS_DB`
 
-logc "Total SMS: $total_sms Delivered: $total_sms_delivered SMS older than 2 days: $total_sms_old"
+logc "Total SMS: $total_sms Delivered: $total_sms_delivered SMS older than 4 days: $total_sms_old"
 logc "Cleanup DB"
 
-echo "delete from SMS where sent is not null;" | sqlite3 $SMS_DB
-echo "delete from SMS where created < datetime('now', '-1 day');" | sqlite3 $SMS_DB
+echo "DELETE from SMS where created < datetime(\"now\", \"-6 hours\") and sent is not null;" | sqlite3 -init <(echo .timeout 1000) $SMS_DB
+echo "DELETE from SMS where created < datetime(\"now\", \"-4 day\") and src_addr='10000' and sent is null;" | sqlite3 -init <(echo .timeout 1000) $SMS_DB
 
 logc "DB size after cleanup: `ls -sh $SMS_DB | awk '{print $1}'`"
