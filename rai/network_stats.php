5d8379b80c9bb312229c83a5eb811ecf83c82bdb
Keith <keith@rhizomatica.org>
2018-02-17 20:25:13 +0100

    RAI: Add Signal Strength graph to the Network Stats page in an iframe

--- rai/network_stats.php
+++ rai/network_stats.php
@@ -3,10 +3,23 @@ require_once('include/header.php');
 require_once('include/menu.php'); 
 
 
-			print_menu('platform'); ?>
+			print_menu('platform');
+			if ( isset($_GET['meas']) && $_GET['meas']==1) {
+			?>
+			<div style="width:90%; height:100%; margin-left: auto; margin-right: auto;">
+			  <iframe style="width:100%; height:1024px" src="http://<?=$_SERVER['HTTP_HOST']?>:8080/" >
+			</div>
+			</div>
+			</body></html>
+			<?
+			exit();
+			}?>
 			<br/><br/><br/>
 			<center>
-			<a href="network_stats.php?a=3h">Last 3 Hours</a> | <a href="network_stats.php?a=12h">Last 12 Hours</a> | <a href="network_stats.php?a=1d">Daily</a> | <a href="network_stats.php?a=1w">Weekly</a> | <a href="network_stats.php?a=1m">Monthly</a> | <a href="network_stats.php?a=1y">Year</a>
+			<a href="network_stats.php?a=3h">Last 3 Hours</a> | <a href="network_stats.php?a=12h">Last 12 Hours</a> |
+			<a href="network_stats.php?a=1d">Daily</a> | <a href="network_stats.php?a=1w">Weekly</a> |
+			<a href="network_stats.php?a=1m">Monthly</a> | <a href="network_stats.php?a=1y">Year</a> |
+			<a href="network_stats.php?meas=1">Meas</a>
 			<br/><br/><br/>
 			<?php
 				$age = (isset($_GET['a'])) ? $_GET['a'] : '12h';
