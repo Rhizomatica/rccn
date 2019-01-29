3a714f0b88e92689d99c7a6e96fb390bfa400df0
Keith Whyte <keith@rhizomatica.org>
2019-01-29 16:32:09 +0000

    Adding initial context:bridge() function to master

--- rccn/context.py
+++ rccn/context.py
@@ -46,6 +46,216 @@ class Context:
         self.billing = modules[2]
         self.configuration = modules[3]
 
+
+    def bridge(self, callee):
+        """ All calls that are progressing arrive here
+            Avoids duplication of code
+
+        :param session: FS session
+        :param destination: Where we bridge the call to
+        """
+        mncc_codec = 'AMR'
+        mncc_port = '5050'
+        inter_port = '5040'
+        endpoints = []
+        bridge_params = ''
+
+        def add_local_ep():
+            codec = mncc_codec
+            bridge_params = ',bridge_early_media=false'
+            endpoint = 'sofia/internal/sip:' + str(callee) + '@' + mncc_ip_address + ':' + mncc_port
+            endpoints.append("[absolute_codec_string='^^:" + codec + "'" + bridge_params + "]" + endpoint)
+
+        def add_sip_ep():
+            sip_endpoint = self.numbering.is_number_sip_connected(self.session, callee)
+            if sip_endpoint:
+                codec = 'PCMA:G729'
+                endpoints.append("[absolute_codec_string='^^:" + codec + "'" + bridge_params + "]" + sip_endpoint)
+
+        def add_internal_ep():
+            endpoint = 'sofia/internalvpn/sip:' + str(callee) + '@' + str(site_ip) + ':' + inter_port
+            bridge_params = ',bridge_early_media=true'
+            codec = 'AMR:G729'
+            endpoints.append("[absolute_codec_string='^^:" + codec + "'" + bridge_params + "]" + endpoint)
+
+        self.session.execute('set', "continue_on_fail="
+                             "DESTINATION_OUT_OF_ORDER,"
+                             "USER_BUSY,"
+                             "NO_ANSWER,"
+                             "NO_ROUTE_DESTINATION,"
+                             "NO_USER_RESPONSE,"
+                             "UNALLOCATED_NUMBER,"
+                             "INVALID_GATEWAY,"
+                             "GATEWAY_DOWN,"
+                             "CALL_REJECTED,"
+                             "NORMAL_TEMPORARY_FAILURE,"
+                             "INVALID_PROFILE,"
+                             "RECOVERY_ON_TIMER_EXPIRE,"
+                             "BEARERCAPABILITY_NOTIMPL,"
+                             "NETWORK_OUT_OF_ORDER")
+
+        # The default for hangup_after_bridge is false but no harm to have it here
+        self.session.execute('set', 'hangup_after_bridge=false')
+        #self.session.execute('set', 'fail_on_single_reject=true')
+        # If we get early media, we'd ignore it.. (in case need late neg.)
+        self.session.execute('set', 'ignore_early_media=false')
+        # can we set the port?
+        #self.session.execute('set', 'remote_media_port=60000')
+        #self.session.execute('set', 'ringback=${us-ring}')
+
+        # Defaults to sending the call to the local SIP to MNCC UA
+        _context = self.session.getVariable('context')
+        log.info("Bridge Context: %s", _context)
+        # build an array of all possible sip endpoints, then bridge.
+
+        if _context == 'OUTBOUND':
+            """
+            OUTBOUND: Call has intl destination.
+            """
+
+            ''' TODO
+            try:
+                codec = self.configuration.get_meta('outbound_codec')
+            except ConfigurationException as e:
+                log.error(e)
+            '''
+            codec = 'G729'
+            try:
+                gw = self.numbering.get_gateway()
+                if gw is None:
+                    log.error('Error in getting the Gateway to use for the call')
+                    self.session.execute('playback', '%s' % self.get_audio_file('INVALID_GATEWAY'))
+                    self.session.hangup('INVALID_GATEWAY')
+                    return
+                log.debug('Use gateway: %s', gw)
+            except NumberingException as numex:
+                log.error(numex)
+                self.session.execute('playback', '%s' % self.get_audio_file('INVALID_GATEWAY'))
+                return False
+            endpoint = 'sofia/gateway/' + gw + '/' + str(callee)
+            bridge_params = ',sip_cid_type=pid'
+            endpoints.append("[absolute_codec_string='^^:" + codec + "'" + bridge_params + "]" + endpoint)
+
+        if _context == 'ROAMING_OUTBOUND':
+            """
+            ROAMING_OUTBOUND: Sending the intl call from a roaming user via their home site, for billing etc.
+            """
+            self.session.execute('set', 'ringback=%(500,500,450,500);%(250,1000,450,500)')
+            self.session.execute('set', 'instant_ringback=true')
+            site_ip = self.numbering.get_site_ip(self.session.getVariable('caller_id_number'))
+            add_internal_ep()
+
+        if (_context == 'INBOUND' or _context == 'LOCAL' or
+                _context == 'INTERNAL_INBOUND' or _context == "ROAMING_LOCAL" or
+                _context == 'SUPPORT'):
+            """
+            INBOUND: Call from Voip Provider
+            LOCAL: from local() to local user.
+            INTERNAL_INBOUND:  call for local called number is originating another site.
+            ROAMING_LOCAL: A local user is calling a foreign user that is here.
+            """
+            self.session.setVariable('effective_caller_id_number', '%s' % self.session.getVariable('caller_id_number'))
+            self.session.setVariable('effective_caller_id_name', '%s' % self.session.getVariable('caller_id_name'))
+            self.session.execute('set', 'ringback=${us-ring}')
+            #self.session.preAnswer()
+            add_local_ep()
+            if use_sip and _context != "ROAMING_LOCAL": # Foreign user will not be (SIP) registered here.
+                add_sip_ep()
+            if _context == "ROAMING_LOCAL": # Also bridge to home in case our info is incorrect.
+                site_ip = self.numbering.get_site_ip(self.destination_number)
+                add_internal_ep()
+
+        if _context == 'INTERNAL' or _context == 'ROAMING_INTERNAL' or _context == "ROAMING_INBOUND":
+            """
+            INTERNAL:           A-leg: Call to another site.
+            ROAMING_INTERNAL:   A-leg: Call from a roaming user (here), is to another roaming user (might be here) FIXME!!
+            ROAMING_INBOUND:    A-leg: Call from VoIP provider to a (local) roaming user.   B-leg: local,sip,remote
+            """
+            try:
+                site_ip = self.numbering.get_current_bts(callee)
+            except NumberingException as ne:
+                # FIXME: Again, we don't know if not exists or other error :(
+                log.error(ne)
+                self.session.execute('playback', '%s' % self.get_audio_file('UNALLOCATED_NUMBER'))
+                self.session.hangup('UNALLOCATED_NUMBER')
+                return
+
+            self.session.setVariable('effective_caller_id_number', '%s' % self.session.getVariable('caller_id_number'))
+            self.session.setVariable('effective_caller_id_name', '%s' % self.session.getVariable('caller_id_number'))
+            self.session.execute('set', 'ringback=%(500,500,450,500);%(250,1000,450,500)')
+            self.session.execute('set', 'instant_ringback=true')
+            self.session.execute('set', 'bridge_early_media=true')
+            self.session.execute('set', 'ignore_early_media=false')
+
+            if _context == 'ROAMING_INTERNAL' or _context == 'ROAMING_INBOUND':
+                self.session.execute('set', 'ringback=${us-ring}')
+                add_local_ep()
+                #add_sip_ep()
+            if site_ip != self.calling_host: # Don't bridge the call back to the origin.
+                add_internal_ep()
+
+        if _context[:8] == 'ROAMING_':
+            self.session.execute('set', "continue_on_fail=true")
+
+        # Now bridge B-leg of call.
+        for ep in endpoints:
+            log.info('Bridging to (%s) EP:\n------------> (\033[92;1m%s\033[0m)',
+                     _context, ep)
+            #log.info('Bridging to (%s) EP: %s' % (e.split('@')[1].split(':')[0], e))
+        bridge_str = ",".join(endpoints)
+        #log.info('%s', bridge_str)
+        self.session.execute('bridge', bridge_str)
+        _orig_disp = self.session.getVariable('originate_disposition')
+        _ep_disp = self.session.getVariable('endpoint_disposition')
+        # Note that if the A leg hangs up, then the last bridge hangup is not from the connected B-leg.
+        _hup_cause = self.session.getVariable('last_bridge_hangup_cause')
+        log.info('Bridge Finished with B-leg of Call, orig_disp(%s) ep_disp(%s) hup_cause(%s)',
+                 _orig_disp, _ep_disp, _hup_cause)
+
+        if _orig_disp == "SUCCESS":
+            if _ep_disp == "ANSWER":
+                return _hup_cause
+            if _ep_disp == "EARLY MEDIA":
+                self.session.hangup(_hup_cause)
+                if _hup_cause != "NORMAL_CLEARING":
+                    return False
+                else:
+                    return True
+        if _orig_disp == "ORIGINATOR_CANCEL":
+            self.session.hangup(_orig_disp)
+            return True
+
+        if _context[:8] == "ROAMING_" and _orig_disp == "UNALLOCATED_NUMBER":
+            # Don't play audio to an incoming roaming call for a number that is
+            # unknown to OsmoHLR, this would kill any another bridge.
+            # Also it might not be correct.
+            self.session.hangup("SUBSCRIBER_ABSENT")
+            return
+        if _context == "INTERNAL_INBOUND" or _context == "ROAMING_INTERNAL" or _context == "ROAMING_LOCAL":
+            # Let the caller side deal with audio feedback
+            self.session.hangup(_hup_cause)
+            return True
+
+        if _context == "SUPPORT":
+            _hup_cause = "NO_ROUTE_DESTINATION"
+
+        # Playback our own audio based on originate disposition.
+        if _orig_disp == "NORMAL_CLEARING" or _orig_disp == "DESTINATION_OUT_OF_ORDER":
+            _audio_f = self.get_audio_file(_hup_cause)
+        else:
+            _audio_f = self.get_audio_file(_orig_disp)
+        if _orig_disp == "RECOVERY_ON_TIMER_EXPIRE" and _context == "OUTBOUND":
+            _audio_f = self.get_audio_file("GATEWAY_DOWN")
+
+        log.debug('Playback to caller: <%s>', _audio_f)
+        if _audio_f != "":
+            #self.session.execute('info')
+            self.session.execute('playback', '%s' % _audio_f)
+            log.debug('Playback Finished.')
+            # Don't hangup here if you want to go back into the inbound loop.
+            if not _context == "INBOUND":
+                self.session.hangup(_orig_disp)
+
     def outbound(self):
         """ Outbound context. Calls to be sent out using the VoIP provider """
 
